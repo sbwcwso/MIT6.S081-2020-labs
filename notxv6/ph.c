24ee4104ed0a7f82511d41e59dccc3a0286b7758
sbwcwso <lijunjie199502@gmail.com>
2025-11-10 11:08:25 +0800

    ready to submit my lab

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,7 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t locks[NBUCKET];
 struct entry {
   int key;
   int value;
@@ -51,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]);
   }
 }
 
@@ -106,6 +109,9 @@ main(int argc, char *argv[])
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
+  }
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
